46de784ef9a56c67ff7d81aef5a36562e6343e47
Basti <thielke.sebastian@gmail.com>
2026-02-14 14:11:37 +0100

    fix install

--- resources/views/emails/monthly_report.blade.php
+++ resources/views/emails/monthly_report.blade.php
@@ -4,8 +4,9 @@
     <title>Monatsbericht</title>
 </head>
 <body>
-    <p>Hallo Chef,</p>
-    <p>anbei erhalten Sie meinen Monatsbericht für {{ $monthName }} {{ $year }}.</p>
-    <p>Mit freundlichen Grüßen,<br>{{ $user->name }}</p>
+    <h1>Monatsbericht für {{ $monthName }} {{ $year }}</h1>
+    <p>Hallo,</p>
+    <p>anbei erhalten Sie den Monatsbericht von {{ $user->name }} für {{ $monthName }} {{ $year }}.</p>
+    <p>Mit freundlichen Grüßen,<br>{{ config('app.name') }}</p>
 </body>
 </html>
