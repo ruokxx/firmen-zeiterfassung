034919a23207195bb8282e431912f6f52eac0ae6
Basti <thielke.sebastian@gmail.com>
2026-02-14 15:11:15 +0100

    fix: add user name to monthly report email fallback

--- resources/views/emails/monthly_report.blade.php
+++ resources/views/emails/monthly_report.blade.php
@@ -5,11 +5,11 @@
 </head>
 <body>
     <div style="font-family: sans-serif; line-height: 1.5;">
-        @if(isset($customBody))
+        @if(isset($customBody) && !empty($customBody))
             {!! nl2br(e($customBody)) !!}
         @else
             <p>Hallo,</p>
-            <p>anbei erhalten Sie den Monatsbericht.</p>
+            <p>anbei erhalten Sie den Monatsbericht von <strong>{{ $user->name ?? 'Mitarbeiter' }}</strong>.</p>
             <p>Mit freundlichen Grüßen,<br>{{ config('app.name') }}</p>
         @endif
     </div>
