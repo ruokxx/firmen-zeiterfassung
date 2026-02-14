0d0e3811f3f40674b8443dd3cd47070796521731
Basti <thielke.sebastian@gmail.com>
2026-02-14 15:07:06 +0100

    fix: robust handling for account approved email

--- resources/views/emails/account_approved.blade.php
+++ resources/views/emails/account_approved.blade.php
@@ -5,7 +5,14 @@
 </head>
 <body>
     <div style="font-family: sans-serif; line-height: 1.5;">
-        {!! nl2br(e($customBody)) !!}
+        @if(isset($customBody))
+            {!! nl2br(e($customBody)) !!}
+        @else
+            <p>Hallo,</p>
+            <p>Dein Account wurde erfolgreich freigeschaltet.</p>
+            <p>Du kannst dich nun einloggen.</p>
+            <p>Mit freundlichen Grüßen,<br>{{ config('app.name') }}</p>
+        @endif
     </div>
 </body>
 </html>
