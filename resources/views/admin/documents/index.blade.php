03afa76d6829bb678a96ead5691db5eccc126d5b
Basti <thielke.sebastian@gmail.com>
2026-02-15 22:17:31 +0100

    bug fixes ,display datum in workday

--- resources/views/admin/documents/index.blade.php
+++ resources/views/admin/documents/index.blade.php
@@ -12,7 +12,7 @@
                     
                     <div class="mb-4 flex justify-between">
                         <h3 class="text-lg font-medium">Verteilte Dokumente</h3>
-                        <a href="{{ route('admin.documents.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        <a href="{{ route('admin.documents.create') }}" class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 active:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                             Neues Dokument verteilen
                         </a>
                     </div>
