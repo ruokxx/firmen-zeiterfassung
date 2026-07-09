9b493373e6d0e2d093c4a58a960755ae0111c79a
Basti <thielke.sebastian@gmail.com>
2026-07-09 09:45:57 +0200

    Fix syntax error when construction site names contain single quotes

--- resources/views/workday/edit.blade.php
+++ resources/views/workday/edit.blade.php
@@ -13,7 +13,7 @@
         </div>
     </x-slot>
 
-    <div class="py-12" x-data='workDayForm({!! $workDay->timeEntries->load("constructionSite")->toJson() !!}, {!! json_encode($sites->pluck("name")) !!})'>
+    <div class="py-12" x-data="workDayForm({{ $workDay->timeEntries->load('constructionSite')->toJson() }}, {{ json_encode($sites->pluck('name')) }})">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6 border border-gray-700">
                 
